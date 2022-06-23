5dfe1ac0c9137f8da745f5eb853b0a738088c745
Bellaaspera <lanasenatorova@gmail.com>
2022-06-23 16:33:20 +0300

    Initial Commit

---
+++ someStuff/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  someStuff
+//
+//  Created by Светлана Сенаторова on 23.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
